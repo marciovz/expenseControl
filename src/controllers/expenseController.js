579984e2b80795a994c223c87a44e7c66ce49b01
marciovz <marciovzim@gmail.com>
2018-05-12 22:33:38 -0300

    Implementado o controller para trabalhar como os metodos do mongoose; Implementado as novas rotas para as despesas (expense)

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,17 +1,39 @@
-const Expense = require('../models/expense');
+const ExpenseModel = require('../models/expense');
 
-let expenseController = {};
+let expenseController = {
 
-expenseController.getAll = (req, res) => {
-  Expense.find()
-    .then( result => res.send( result ))
-    .catch( error => res.status(401).send({ "error": "Could not get expenses" }));
-};
+  create: (req, res) => {
+    ExpenseModel.create(req.body)
+      .then(expense => expense.save().then(res.json(expense)) )
+      .catch(error => {
+        console.log(`expenseModel:  ${error}`);
+        res.status(401).send({ error: "Could not insert a new expense" });
+    });
+  },
 
-expenseController.create = (req, res) => {
-  Expense.insert(req.body)
-    .then(result => res.send( result ))
-    .catch(error => res.status(401).send({ "error": "Could not insert a new expense" }));
+  getAll: (req, res) => {
+    ExpenseModel.find()
+      .then(expenses => res.json(expenses))
+      .catch(error => res.status(401).send({ error: "Could not get expenses" }));
+  },
+
+  getById: (req, res) => {
+    ExpenseModel.findById(req.params.id)
+      .then(result => res.send( result ))
+      .catch(error => res.status(401).send({ error: "Could not get the expense" }));
+  },
+
+  update: (req, res) => {
+    ExpenseModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(expense => res.send( expense ))
+      .catch(error => res.status(401).send({ error: "Could not update the expense" }));
+  },
+
+  remove: (req, res) => {
+    ExpenseModel.findByIdAndRemove(req.params.id)
+      .then(expense => res.send( expense ))
+      .catch(error => res.status(401).send({ error: "Could not delete the expense" }));
+  }
 };
 
 module.exports = expenseController;
--- src/routes/expenseRouter.js
+++ src/routes/expenseRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const expenseController = require('../controllers/expenseController');
 
-router.get('/', expenseController.getAll);
 router.post('/', expenseController.create);
+router.get('/', expenseController.getAll);
+router.get('/:id', expenseController.getById);
+router.put('/:id', expenseController.update);
+router.delete('/:id', expenseController.remove);
 
 module.exports = router;
