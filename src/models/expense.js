b0f9727a5e1bbf5284438b8dcd405ef8f6da3518
marciovz <marciovzim@gmail.com>
2018-05-12 22:30:56 -0300

    Implementado modelo de despesas (models/expense) para usar o mongoose; Redefinido as propriedades do modelo de despesas;

--- src/models/expense.js
+++ src/models/expense.js
@@ -1,15 +1,35 @@
-let Expense = {};
-let data = [];
+const db = require('../database/db');
 
-// return all expenses
-Expense.find = async() => {
-  return await data;
-};
+const schema = db.Schema({
+  account: {
+    type: Number,
+    required: true
+  },
+  provider: {
+    type: String,
+    required: false
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: Number,
+    required: true
+  },
+  dateExpense: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: Number,
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
 
-// insert a new expense
-Expense.insert = async(newExpense) => {
-  await data.push(newExpense);
-  return newExpense;
-};
 
-module.exports = Expense;
+module.exports = db.model('expense', schema);
